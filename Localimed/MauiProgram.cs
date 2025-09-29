81f814273a6e3eabe6c606fe2dbd678e6ea12247
zerowavez <lucastorinopam@gmail.com>
2025-09-29 16:29:17 -0300

    Adição da fonte do logotipo (Montserrat)

--- Localimed/MauiProgram.cs
+++ Localimed/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace Localimed
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Bold.ttf", "Montserrat");
                 });
 
 #if DEBUG
