2562803b2ddd6fdc6985a69bdc8e9705e43e25ce
Benjamin <benbenjamim936@gmail.com>
2025-09-30 09:34:52 -0300

    Incluindo as páginas de remoção e inserção views

--- Localimed/Views/HomePage.xaml.cs
+++ Localimed/Views/HomePage.xaml.cs
@@ -7,4 +7,21 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
 	}
 
+	//Redireciona até a tela de inserir medicamentos da nossa view
+	private async void OnInserirMedicamentosClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///InserirMedicamentos");
+	}
+
+    //Redireciona até a tela de remover medicamentos da nossa view
+    private async void OnRemoverMedicamentosClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///RemoverMedicamentos");
+	}
+
+	private async void OnVisualizarEstoqueClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///VisualizarEstoque");
+	}
+
 }
---
+++ Localimed/Views/InserirMedicamentos.xaml.cs
@@ -0,0 +1,9 @@
+namespace Localimed.Views;
+
+public partial class InserirMedicamentos : ContentPage
+{
+	public InserirMedicamentos()
+	{
+		InitializeComponent();
+	}
+}
